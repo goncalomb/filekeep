5e4bc90f6f13e8e512dc1808f687555eea72dc3c
Gonçalo Baltazar <me@goncalomb.com>
2017-09-26 15:55:28 +0100

    Add proper command-line arguments

--- filekeep/__main__.py
+++ filekeep/__main__.py
@@ -1,21 +1,36 @@
-import os, sys
+import os
+import argparse
 
 from filekeep.collection import Collection
 
 def execute():
+    parser = argparse.ArgumentParser(prog='filekeep')
+    subparsers = parser.add_subparsers(title='commands', dest='command')
+
+    parser_create = subparsers.add_parser('create', description='create collection')
+    parser_export = subparsers.add_parser('export', description='export data')
+    parser_export.add_argument('--format', default='sha1sum', choices=['sha1sum'], help='export format')
+
+    args = parser.parse_args()
+
     col = Collection(os.getcwd())
-    if col.exists:
-        if len(sys.argv) == 2 and sys.argv[1] == "dump":
-            col.print_sha1sum()
-        else:
+
+    if args.command == 'create':
+        if col.exists:
             print("collection exists")
-            print(str(col.size()) + " bytes")
-            print("use 'filekeep dump' to print in sha1sum format")
+        else:
+            print("creating collection")
+            col.create_from_path()
+            col.write_data()
+            print("done")
+    elif args.command == 'export' and col.exists:
+        if args.format == 'sha1sum':
+            col.print_sha1sum()
+    elif col.exists:
+        print("collection exists")
+        print(str(col.size()) + " bytes")
     else:
-        print("creating collection")
-        col.create_from_path()
-        col.write_data()
-        print("done")
+        print("collection not created")
 
 if __name__ == "__main__":
     execute()

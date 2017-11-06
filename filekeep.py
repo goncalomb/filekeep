37bcfa48eca1478b03b7ce16e9fca0bad3fa4e61
Gonçalo Baltazar <me@goncalomb.com>
2017-11-06 19:58:49 +0000

    Add setup.py

--- filekeep.py
+++
@@ -1,3 +0,0 @@
-import runpy
-
-runpy.run_module("filekeep", run_name="__main__")
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='filekeep',
+    version='0.0.0',
+    author='Gonçalo Baltazar',
+    author_email='hello@goncalomb.com',
+    packages=['filekeep'],
+    scripts=['bin/filekeep'],
+)
